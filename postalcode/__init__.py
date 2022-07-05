cc2b4fdea6c17aec7b3a7dc56abef07a289bd904
Eul Rho <qkekshdmf777@gmail.com>
2022-07-05 13:53:11 +0900

    (Fix)application excution part

--- postalcode/__init__.py
+++ postalcode/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-from .views import main_views
-app.register_blueprint(main_views.bp)
+    from .views import main_views
+    app.register_blueprint(main_views.bp)
 
-if __name__=='__main__':
-    app.run(debug=True)
+    return app
--- postalcode/run.py
+++ postalcode/run.py
@@ -1,3 +1,5 @@
-from postalcode import app
+from postalcode import create_app
 
-app.run(host="0.0.0.0")     # 외부에서 접근가능케 한다.
+app = create_app('/path/to/config/cfg')
+app.run(debug=True)     # 디버그 모드. 어플리케이션을 실행할 때 파라미터로 넘겨줌
+# app.run(host="0.0.0.0")     # 외부에서 접근가능케 한다.
