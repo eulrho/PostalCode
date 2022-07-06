605cd2d5954215341266f27c2b32bd5c0e003fef
Eul Rho <sunriselss19@gmail.com>
2022-07-06 17:14:14 +0900

    (Fix)merge navbar.html and search.html

--- postalcode/views/main_views.py
+++ postalcode/views/main_views.py
@@ -4,10 +4,14 @@ from postalcode.mod_dbconn import Database
 bp = Blueprint('main', __name__, url_prefix='/')    # blueprint 객체 생성
 
 @bp.route('/')    # 라우팅 함수
-def select():
-    db_class = Database()
+# def select():
+#     db_class = Database()
+#
+#     # distinct : 중복 데이터 제거
+#     sql = "SELECT DISTINCT postalcode FROM postalcode_db.postalcode LIMIT 10"
+#     row = db_class.executeAll(sql)
+#
+#     return render_template('db.html', resultData=row)
 
-    sql = "SELECT DISTINCT postalcode FROM postalcode_db.postalcode LIMIT 10"
-    row = db_class.executeAll(sql)
-
-    return render_template('db.html', resultData=row)
+def home():
+    return render_template('home.html')
