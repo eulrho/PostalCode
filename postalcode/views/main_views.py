0e8dfc13a781e1ca59b4150ad470dbc0dba0c109
Eul Rho <sunriselss19@gmail.com>
2022-07-12 14:13:20 +0900

    (Fix)problem about body and query string

--- postalcode/views/main_views.py
+++ postalcode/views/main_views.py
@@ -1,32 +1,21 @@
-from flask import Blueprint, render_template, request, jsonify, make_response
+import json
+
+from flask import Blueprint, render_template, request, jsonify
 from postalcode.mod_dbconn import Database
 
-bp = Blueprint('main', __name__, url_prefix='/')    # blueprint 객체 생성
+bp = Blueprint('main', __name__, url_prefix='/')  # blueprint 객체 생성
 
+
+# Json data를 클라이언트->서버로 전달되지 않을 때 발생되는 오류 처리(Bad Request)
 # 빈 curly brackets 를 반환하는 function
 def on_json_loading_failed_return_dict(e):
-	return {}
+    return {}
 
-@bp.route('/', methods=['GET'])    # 라우팅 함수
-# def home():
-#     return render_template('home.html')
-#
-#     db_class = Database()
-#     data = request.get_json()
-#     # data = {"input_pc":'256'}
-#     keyword = data['input_pc']
-#     # data = '"256"'
-#     # distinct : 중복 데이터 제거
-#     sql = "SELECT DISTINCT postalcode FROM postalcode_db.postalcode WHERE MATCH(postalcode) AGAINST(" + keyword + " IN BOOLEAN MODE) LIMIT 10"
-#     # sql = "SELECT DISTINCT postalcode FROM postalcode_db.postalcode WHERE MATCH(postalcode) AGAINST('"256"') IN BOOLEAN MODE) LIMIT 10"
-#     row = db_class.executeAll(sql)
-#     # return render_template('db.html', resultData=row)
-#     # result = jsonify(row)
-#     res = make_response(keyword)
-#     return res
-#     # return jsonify(row)
-# def home():
-#     return render_template('home.html')
+
+@bp.route('/', methods=['GET'])  # 라우팅 함수
+def home():
+    return render_template('home.html')
+
 
 @bp.route('/ajax', methods=['GET'])
 def select():
@@ -34,15 +23,18 @@ def select():
     request.on_json_loading_failed = on_json_loading_failed_return_dict
 
     db_class = Database()
-    data = request.get_json()
+    data = request.args.get('input_pc')
     print(data)
-    # data = {"input_pc":'256'}
-    inputData = data['input_pc']
-    print(inputData)
-    # data = '"256"'
+
     # distinct : 중복 데이터 제거
-    sql = "SELECT DISTINCT postalcode FROM postalcode_db.postalcode WHERE MATCH(postalcode) AGAINST(" + inputData + " IN BOOLEAN MODE) LIMIT 10"
-    # sql = "SELECT DISTINCT postalcode FROM postalcode_db.postalcode WHERE MATCH(postalcode) AGAINST('"256"') IN BOOLEAN MODE) LIMIT 10"
+    sql = "SELECT DISTINCT postalcode FROM postalcode_db.postalcode WHERE MATCH(postalcode) AGAINST(" + str(data) + " IN BOOLEAN MODE) LIMIT 10"
     row = db_class.executeAll(sql)
-    return make_response(jsonify(row))
-    # return jsonify(row)
+
+    return jsonify(row)
+    return data
+
+@bp.route('/testbody', methods=['GET'])
+def testBody():
+    data = request.get_data()
+    print(data)
+    return data
