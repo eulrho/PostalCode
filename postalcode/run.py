10ccec722930f0a3d9bd7a517ea86a553d0a352f
Eul Rho <sunriselss19@gmail.com>
2022-07-20 13:35:30 +0900

    (Add)app run option

--- postalcode/run.py
+++ postalcode/run.py
@@ -1,5 +1,5 @@
 from postalcode import create_app
 
 app = create_app('/path/to/config/cfg')
-app.run(debug=True)     # 디버그 모드. 어플리케이션을 실행할 때 파라미터로 넘겨줌
+app.run(debug=True, threaded=True)     # 디버그 모드. 어플리케이션을 실행할 때 파라미터로 넘겨줌
 # app.run(host="0.0.0.0")     # 외부에서 접근가능케 한다.
